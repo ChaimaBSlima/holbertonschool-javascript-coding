302d3f544da07eea6e3309af4905a402f979c0b3
ChaimaBSlima <chaimabslima@outlook.fr>
2024-05-27 10:52:08 +0100

    add comments

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,3 +1,13 @@
-
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
 const displayMessage = (message) => console.log(message);
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
 module.exports = displayMessage;
